df0737889f4038e741db51fdfa75b3e89e2144be
余江 <597682780@qq.com>
2021-04-15 10:58:18 +0800

    init‘ ;

---
+++ api/src/main/java/api/IHelloService.java
@@ -0,0 +1,14 @@
+package api;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "spring-cloud-producer",path = "hello")
+public interface IHelloService {
+
+	@GetMapping("/sayHello")
+	String sayHello(@RequestParam(value = "name") String name);
+
+}
---
+++ service/src/main/java/springboots/service/HelloController.java
@@ -0,0 +1,17 @@
+package springboots.service;
+
+
+import api.IHelloService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController implements IHelloService {
+
+
+    @Override
+    public String sayHello(String name) {
+        return "hello:"+name+"I am port 9002" ;
+    }
+}
